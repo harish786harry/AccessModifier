baec600c8278eb39f3f264c079810ac85e1b76b7
harish786harry <harish786harry@gmail.com>
2022-02-09 21:42:32 +0530

    types of inheritance

---
+++ src/hk/TypesofInheritance.java
@@ -0,0 +1,25 @@
+package hk;
+class Parent1 //Parent
+{
+	void sum()
+	{
+		
+	}
+}
+class Parent2 //child1 of parent1
+{
+	
+}
+class Parent3 extends Parent2  //child2
+{
+	void sum()
+	{
+		
+	}
+}
+public class TypesofInheritance {
+	public static void main(String[] args) {
+		
+	}
+
+}
